cf6a44e34801a455866662798c0f5af02d68ba51
Katharine Berry <katharine@getpebble.com>
2014-12-05 12:26:46 -0800

    Make console.log more javascripty.

--- javascript/console.py
+++ javascript/console.py
@@ -5,7 +5,7 @@ import PyV8 as v8
 
 class Console(v8.JSClass):
     def log(self, *params):
-        print ' '.join([str(x) for x in params])
+        print ' '.join([x.toString() if hasattr(x, 'toString') else str(x) for x in params])
 
     def warn(self, *params):
         self.log(params)
