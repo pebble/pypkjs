87301a80ed5602c3ab1412319eada834615d41a8
Katharine Berry <katharine@getpebble.com>
2014-12-08 15:41:16 -0800

    Support switching between multiple apps.

--- jskit.py
+++ jskit.py
@@ -4,6 +4,7 @@ monkey.patch_all()
 import zipfile
 import json
 import gevent
+import collections
 from uuid import UUID
 
 import javascript.runtime
@@ -11,45 +12,50 @@ from pebble_manager import PebbleManager
 
 
 class Runner(object):
-    def __init__(self, qemu, pbw):
+    PBW = collections.namedtuple('PBW', ('uuid', 'src', 'manifest'))
+
+    def __init__(self, qemu, pbws):
         self.qemu = qemu
         self.pebble = PebbleManager(qemu)
         self.pebble.handle_start = self.handle_start
         self.pebble.handle_stop = self.handle_stop
+        self.pbws = {}
+        self.running_uuid = None
         self.js = None
-        self.manifest = None
-        self.uuid = None
-        self.src = None
-        self.load_pbw(pbw)
+        self.load_pbws(pbws)
 
-    def load_pbw(self, pbw_path):
-        with zipfile.ZipFile(pbw_path) as z:
-            appinfo = z.open('appinfo.json').read()
-            self.src = z.open('pebble-js-app.js').read().decode('utf-8')
-        self.manifest = json.loads(appinfo)
-        self.uuid = UUID(self.manifest['uuid'])
-        print "ready with app %s" % self.uuid
+    def load_pbws(self, pbws):
+        for pbw_path in pbws:
+            with zipfile.ZipFile(pbw_path) as z:
+                appinfo = z.open('appinfo.json').read()
+                src = z.open('pebble-js-app.js').read().decode('utf-8')
+            manifest = json.loads(appinfo)
+            uuid = UUID(manifest['uuid'])
+            self.pbws[uuid] = self.PBW(uuid, src, manifest)
+        print "ready with apps %s" % ', '.join(map(str, self.pbws.keys()))
 
     def handle_start(self, uuid):
-        if uuid == self.uuid:
-            print "starting js"
-            self.start_js()
+        if uuid in self.pbws:
+            print "starting js for %s" % uuid
+            self.start_js(self.pbws[uuid])
 
     def handle_stop(self, uuid):
-        if uuid == self.uuid:
+        if uuid == self.running_uuid:
             print "stopping js"
             self.stop_js()
 
-    def start_js(self):
+    def start_js(self, pbw):
         self.stop_js()
-        self.js = javascript.runtime.JSRuntime(self.pebble, self.manifest)
-        gevent.spawn(self.js.run, self.src)
+        self.running_uuid = pbw.uuid
+        self.js = javascript.runtime.JSRuntime(self.pebble, pbw.manifest)
+        gevent.spawn(self.js.run, pbw.src)
 
 
     def stop_js(self):
         if self.js is not None:
             self.js.stop()
             self.js = None
+            self.running_uuid = None
 
     def run(self):
         print 'connecting'
@@ -73,5 +79,5 @@ def run_pbw(qemu, pbw_path):
 
 if __name__ == "__main__":
     # run_script('localhost:12344', open('pebble-js-app.js', 'r').read().decode('utf-8'))
-    runner = Runner('localhost:12344', '/Users/katharine/Downloads/twc (5).pbw')
+    runner = Runner('localhost:12344', ['/Users/katharine/Downloads/twc (5).pbw', '/Users/katharine/Downloads/Love_Weather-3.pbw'])
     runner.run()
