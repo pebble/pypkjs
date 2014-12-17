7d0050ae989ffcc707539263f2ff216efc78558f
Katharine Berry <katharine@getpebble.com>
2014-12-17 14:25:31 -0800

    Use GeoIP for geolocation.

--- javascript/navigator/geolocation.py
+++ javascript/navigator/geolocation.py
@@ -2,6 +2,9 @@ __author__ = 'katharine'
 
 import PyV8 as v8
 import time
+import requests
+import pygeoip
+import os.path
 
 
 class Position(v8.JSClass):
@@ -22,11 +25,27 @@ class Geolocation(v8.JSClass):
     def __init__(self, runtime):
         self.__runtime = runtime
 
+    def _get_position(self, success, failure):
+        try:
+            resp = requests.get('http://ip.42.pl/raw')
+            resp.raise_for_status()
+            ip = resp.text
+            gi = pygeoip.GeoIP('%s/GeoLiteCity.dat' % os.path.dirname(__file__))
+            record = gi.record_by_addr(ip)
+            if record is None:
+                if callable(failure):
+                    failure()
+        except (requests.RequestException, pygeoip.GeoIPError):
+            if callable(failure):
+                failure()
+        else:
+            self.__runtime.enqueue(success, Position(Coordinates(record['longitude'], record['latitude'], 1000), int(time.time() * 1000)))
+
     def getCurrentPosition(self, success, failure=None, options=None):
-        self.__runtime.enqueue(success, Position(Coordinates(-122.1583327, 37.4401251, 10), int(time.time() * 1000)))
+        self.__runtime.group.spawn(self._get_position, success, failure)
 
     def watchPosition(self, success, failure=None, options=None):
-        self.__runtime.enqueue(success, Position(Coordinates(-122.1583327, 37.4401251, 10), int(time.time() * 1000)))
+        self.__runtime.group.spawn(self._get_position, success, failure)
         return 42
 
     def clearWatch(self, thing):
